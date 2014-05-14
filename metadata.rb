109bc88adb742363d62fcaecd3bcf39b7277d83b
Rune Madsen <rune@runemadsen.com>
2014-05-14 15:20:32 -0400

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rune@runemadsen.com'
 license          'Apache 2.0'
 description      'Do stuff with .env files'
 long_description 'Do stuff with .env files'
-version          '0.0.1'
+version          '0.0.2'
 %w[ debian ubuntu centos redhat fedora scientific suse amazon].each do |os|
   supports os
 end
